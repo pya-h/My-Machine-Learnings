3c4b3f7ad6b37c157bc1f8ecd91e521bb3187510
pya-h <p4ya.py@gmail.com>
2023-09-01 13:08:42 +0330

    predicting

--- 02.simple-linear-regression/SLR.py
+++ 02.simple-linear-regression/SLR.py
@@ -5,6 +5,18 @@ import pandas as pd
 
 # data preprocessing
 dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, :-1]
+x = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
 
+from sklearn.model_selection import train_test_split
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, train_size=2/3, test_size=1/3)
+
+# no need to feature scaling and n/a imputing and encoding, according to dataset
+
+# Fitting simple linear regression to training set
+from sklearn.linear_model import LinearRegression
+linear_regressor = LinearRegression()
+linear_regressor.fit(xTrain, yTrain)
+
+# use regressor to make predictions on xTest
+yPredicted = linear_regressor.predict(xTest)
